1e73625ceb5c13c18c81283501f4cdefe48f40ea
Nikhil-Koganti <nikhil.sunny731@gmail.com>
2023-04-01 12:23:24 -0700

    Modified the planets and moons pages for grid layouts

--- src/pages/moons/index.tsx
+++ src/pages/moons/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { getStellarData } from "../../api/api";
 import Link from "next/link";
 import { GetStaticProps } from "next";
+import styles from "@/styles/Home.module.css";
 
 interface Moon {
   moon_id: string;
@@ -28,25 +29,29 @@ export default function MoonsPage({ moons }: MoonsProps) {
   const groupedMoons = groupMoonsByPlanet(moons);
 
   return (
-    <div className="w-80">
-      <h1>Moons</h1>
-      {Object.entries(groupedMoons).map(([planetId, planetMoons]) => (
-        <div key={planetId}>
-          <Link href={`/planets/${planetId}`} className="font-bold text-lg">
-            {planetId}
-          </Link>
-          <ul className="flex flex-row">
-            {planetMoons.map((moon) => (
-              <li key={moon.moon_id}>
-                <Link href={`/moons/${moon.moon_id}`}>
-                  <h3>{moon.englishName}</h3>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
-    </div>
+    <main className="h-screen flex flex-col font-mono relative">
+      <div className="bg-gradient-to-b from-black to-slate-700 flex-1 flex flex-col justify-start items-center p-8">
+        <h1 className="font-bold text-lg text-white mb-4">Moons Directory</h1>
+        <section className={`relative flex-1 ${styles.grid} grid grid-cols-4 gap-6 text-white`}>
+          {Object.entries(groupedMoons).map(([planetId, planetMoons]) => (
+            <div key={planetId}>
+              <Link href={`/planets/${planetId}`} className="font-bold text-lg">
+                {planetId}
+              </Link>
+              <div className="moon-grid">
+                {planetMoons.map((moon) => (
+                  <Link key={moon.moon_id} href={`/moons/${moon.moon_id}`}>
+                    <div className="moon-item">
+                      <h3 className="text-center">{moon.englishName}</h3>
+                    </div>
+                  </Link>
+                ))}
+              </div>
+            </div>
+          ))}
+        </section>
+      </div>
+    </main>
   );
 }
 
--- src/pages/planets/index.tsx
+++ src/pages/planets/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { getStellarData } from "../../api/api";
 import { GetStaticProps, GetStaticPaths } from "next";
 import Link from "next/link";
+import styles from "@/styles/Home.module.css";
 
 interface Planet {
   planet_id: string;
@@ -14,15 +15,21 @@ interface PlanetProps {
 
 export default function PlanetsDirectory({ planets }: PlanetProps) {
   return (
-    <div>
-      <h1 className="font-bold text-lg">Planets</h1>
-      {planets.map((planet) => (
-        <div key={planet.planet_id}>
-          <Link href={`planets/${planet.planet_id}`}>{planet.englishName}</Link>
-        </div>
-      ))}
-    </div>
+    <main className="h-screen flex flex-col font-mono relative">
+      <div className="bg-gradient-to-b from-black to-slate-700 flex-1 flex flex-col justify-start items-center p-8">
+        <h1 className="font-bold text-lg text-white mb-4">Planets Directory</h1>
+        <section className={`relative flex-1 ${styles.grid} grid grid-cols-3 gap-6`}>
+          {planets.map((planet) => (
+            <div key={planet.planet_id} className="grid-item bg-gray-800 rounded-lg p-8 grid-col span-2">
+              <Link href={`planets/${planet.planet_id}`} className="text-white flex justify-center">{planet.englishName}</Link>
+            </div>
+          ))}
+        </section>
+      </div>
+    </main>
   );
+  
+  
 }
 
 export const getStaticProps: GetStaticProps<PlanetProps> = async () => {
